305bc8a72ec069f2f72dc28a2e4b0579de10eaf1
nourab <nurab7091@gmail.com>
2024-03-15 23:31:41 +0300

    comm

--- courseservice/src/main/java/com/courserservice/courseservice/mapper/CourseMapper.java
+++ courseservice/src/main/java/com/courserservice/courseservice/mapper/CourseMapper.java
@@ -1,4 +1,27 @@
 package com.courserservice.courseservice.mapper;
 
+import com.courserservice.courseservice.dto.CourseDto;
+import com.courserservice.courseservice.entity.Course;
+
 public class CourseMapper {
+    public  static CourseDto mapToCourseDto(Course course){
+        return new CourseDto(
+                course.getId(),
+                course.getCourseName(),
+                course.getCourseType(),
+                course.getType(),
+                course.getDescription(),
+                course.getAttachmentPath()
+        );
+    }
+    public static Course mapToUser(CourseDto courseDto){
+        return new Course(
+                courseDto.getId(),
+                courseDto.getCourseName(),
+                courseDto.getCourseType(),
+                courseDto.getType(),
+                courseDto.getDescription(),
+                courseDto.getAttachmentPath()
+        );
+    }
 }
