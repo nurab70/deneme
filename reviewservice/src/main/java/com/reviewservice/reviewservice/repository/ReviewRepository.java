a1b29d47e1deb53b8a959d44698884495d720a5b
nurab70 <134394221+nurab70@users.noreply.github.com>
2024-03-18 14:18:14 +0300

    Delete reviewservice directory

--- reviewservice/src/main/java/com/reviewservice/reviewservice/repository/ReviewRepository.java
+++
@@ -1,7 +0,0 @@
-package com.reviewservice.reviewservice.repository;
-
-import com.reviewservice.reviewservice.entity.Review;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReviewRepository extends JpaRepository<Review,Long> {
-}
--- reviewservice/src/main/java/com/reviewservice/reviewservice/service/ReviewService.java
+++
@@ -1,7 +0,0 @@
-package com.reviewservice.reviewservice.service;
-
-import com.reviewservice.reviewservice.dto.ReviewDto;
-
-public interface ReviewService {
-    ReviewDto createReview(ReviewDto reviewDto);
-}
